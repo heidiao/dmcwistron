f96188fdaeff56730eabe97023240ccc0ba66666
heidi <heidi_ao@wistron.com>
2014-02-18 09:46:18 +0800

    remove unuse file

--- gen/R.java.d
+++
@@ -1,4 +0,0 @@
-/home/heidi/work/upnp/heidi/gen/wistron/upnp/heidi/R.java \
- : /home/heidi/work/upnp/heidi/res/layout/activity_main.xml \
-/home/heidi/work/upnp/heidi/res/values/strings.xml \
-/home/heidi/work/upnp/heidi/bin/AndroidManifest.xml \
