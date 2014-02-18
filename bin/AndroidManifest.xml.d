32b29c7cd34f54e6ba9da9cdebcdc34f9c6b10b4
heidi <heidi_ao@wistron.com>
2014-02-18 09:47:57 +0800

    remove unuse file

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/heidi/work/upnp/heidi/bin/AndroidManifest.xml : \
-/home/heidi/work/upnp/heidi/AndroidManifest.xml \
--- bin/wistronDmr.ap_.d
+++
@@ -1,4 +0,0 @@
-/home/heidi/work/upnp/heidi/bin/wistronDmr.ap_ \
- : /home/heidi/work/upnp/heidi/res/layout/activity_main.xml \
-/home/heidi/work/upnp/heidi/res/values/strings.xml \
-/home/heidi/work/upnp/heidi/bin/AndroidManifest.xml \
